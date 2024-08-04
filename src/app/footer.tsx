14531f8552aeb9cd2923b8d6d13ef799cb3c8fd9
kush1sharma <159148748+kush1sharma@users.noreply.github.com>
2024-08-05 00:41:10 +0530

    Initial Commit

---
+++ src/app/footer.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+import styles from './footer.module.css';
+
+const Footer: FC = () => {
+  return (
+    <footer className={styles.footer}>
+      <p>Made by 💜 Kushagra Sharma
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
